a6b53531f7190ec15d7d1795ce67d0c72abba13f
Kaden Krempa <krempakaden@gmail.com>
2017-12-17 20:05:44 -0500

    added python files to web caesar

---
+++ main.py
@@ -0,0 +1,50 @@
+from flask import Flask, request
+from caesar import rotate_string
+
+app = Flask(__name__)
+app.config ['DEBUG'] = True
+
+form = """
+    <!DOCTYPE html>
+    
+    <html>
+        <head>
+            <style>
+                form {
+                    background-color: #eee; 
+                    padding: 20px; 
+                    margin: 0 auto; 
+                    width: 540px; 
+                    font: 16px sans-serif; 
+                    border-radius: 10px;
+                }
+                textarea {
+                    margin: 10px 0;
+                    width: 540px;
+                    height: 120px;
+                }
+            </style>
+        </head>
+        <body>
+            <form action="/hello" method="post">
+            <label for= "rotate-by">Rotate By:</label>
+            <input type = "text" name="rot" value="0"/>
+            <textarea name="text"></textarea>
+            <input type= "submit" />
+        </body>
+    </html>
+"""
+
+@app.route("/", methods=['POST'])
+def encrypt():
+    rot = 
+    text =
+
+    return "<h1> " + rotate_string + "</h1>"
+
+
+@app.route("/")
+def index():
+        return form
+
+app.run()
